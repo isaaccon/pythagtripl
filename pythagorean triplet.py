dbbc70b2987e8e6af7c71d6963ea9f56444dc06c
isaaccon <137878848+isaaccon@users.noreply.github.com>
2023-06-27 09:58:50 +0100

    Add files via upload

--- pythagorean triplet.py
+++ pythagorean triplet.py
@@ -1,9 +1,12 @@
-for a in range(1,1001):
-    for b in range(1,1001):
-        for c in range(1,1001):
-            if a**2 + b**2 == c**2:
-                print(a, b, c, c**2)
-                if a + b + c == 1000:
-                    print(a*b*c)
-                    break
+def solver():
+    for a in range(1,1001):
+        for b in range(1,1001):
+            for c in range(1,1001):
+                if a**2 + b**2 == c**2:
+                    print(a, b, c, c**2)
+                    if a + b + c == 1000:
+                        print(a*b*c)
+                        return (a*b*c)
+print("Script Started")
+print(solver())
     
